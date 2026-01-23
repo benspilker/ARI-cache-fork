3ae0b2b0da0160726a0c0dc1fb31e6d38312730c
Ben Spilker <bspilker@presidio.com>
2026-01-23 16:06:55 -0500

    corrected excel ordering issue

--- Modules/Private/3.ReportingFunctions/StyleFunctions/Start-ARIExcelOrdening.ps1
+++ Modules/Private/3.ReportingFunctions/StyleFunctions/Start-ARIExcelOrdening.ps1
@@ -65,18 +65,27 @@ function Start-ARIExcelOrdening {
 
         Foreach ($Ord in $Order0) {
             if ($null -ne $Ord -and $null -ne $Ord.Index -and $null -ne $Ord.Name) {
-                if ($Loop -ne 0 -and $Order0.Count -gt ($Loop - 1) -and $null -ne $Order0[$Loop - 1] -and $null -ne $Order0[$Loop - 1].Name) {
-                    try {
-                        $Worksheets.MoveAfter($Ord.Name, $Order0[$Loop - 1].Name)
-                    } catch {
-                        Write-Debug "  Warning: Could not move sheet $($Ord.Name): $_"
+                $targetSheet = $Excel.Workbook.Worksheets[$Ord.Name]
+                if ($null -ne $targetSheet) {
+                    if ($Loop -ne 0 -and $Order0.Count -gt ($Loop - 1) -and $null -ne $Order0[$Loop - 1] -and $null -ne $Order0[$Loop - 1].Name) {
+                        try {
+                            $afterSheet = $Excel.Workbook.Worksheets[$Order0[$Loop - 1].Name]
+                            if ($null -ne $afterSheet) {
+                                $targetSheet.Position = $afterSheet.Position + 1
+                            }
+                        } catch {
+                            Write-Debug "  Warning: Could not move sheet $($Ord.Name): $_"
+                        }
                     }
-                }
-                if ($Loop -eq 0 -and $null -ne $Order[0] -and $null -ne $Order[0].Name) {
-                    try {
-                        $Worksheets.MoveAfter($Ord.Name, $Order[0].Name)
-                    } catch {
-                        Write-Debug "  Warning: Could not move sheet $($Ord.Name): $_"
+                    if ($Loop -eq 0 -and $null -ne $Order[0] -and $null -ne $Order[0].Name) {
+                        try {
+                            $afterSheet = $Excel.Workbook.Worksheets[$Order[0].Name]
+                            if ($null -ne $afterSheet) {
+                                $targetSheet.Position = $afterSheet.Position + 1
+                            }
+                        } catch {
+                            Write-Debug "  Warning: Could not move sheet $($Ord.Name): $_"
+                        }
                     }
                 }
             }
@@ -87,75 +96,24 @@ function Start-ARIExcelOrdening {
     Write-Debug ((get-date -Format 'yyyy-MM-dd_HH_mm_ss')+' - '+'Validating if Advisor and Policies are included.')
     # Safely check for worksheets with Name property
     if ($null -ne $Worksheets) {
-        $advisorSheet = $Worksheets | Where-Object { $null -ne $_ -and $null -ne $_.Name -and $_.Name -eq 'Advisor'} | Select-Object -First 1
-        if ($advisorSheet) {
-            try {
-                $Worksheets.MoveAfter('Advisor', 'Overview')
-            } catch {
-                Write-Debug "  Warning: Could not move Advisor sheet: $_"
-            }
-        }
-        
-        $policySheet = $Worksheets | Where-Object { $null -ne $_ -and $null -ne $_.Name -and $_.Name -eq 'Policy'} | Select-Object -First 1
-        if ($policySheet) {
-            try {
-                $Worksheets.MoveAfter('Policy', 'Overview')
-            } catch {
-                Write-Debug "  Warning: Could not move Policy sheet: $_"
-            }
-        }
-        
-        $securitySheet = $Worksheets | Where-Object { $null -ne $_ -and $null -ne $_.Name -and $_.Name -eq 'Security Center'} | Select-Object -First 1
-        if ($securitySheet) {
-            try {
-                $Worksheets.MoveAfter('Security Center', 'Overview')
-            } catch {
-                Write-Debug "  Warning: Could not move Security Center sheet: $_"
-            }
-        }
-        
-        $quotaSheet = $Worksheets | Where-Object { $null -ne $_ -and $null -ne $_.Name -and $_.Name -eq 'Quota Usage'} | Select-Object -First 1
-        if ($quotaSheet) {
-            try {
-                $Worksheets.MoveAfter('Quota Usage', 'Overview')
-            } catch {
-                Write-Debug "  Warning: Could not move Quota Usage sheet: $_"
-            }
-        }
-        
-        $advisorScoreSheet = $Worksheets | Where-Object { $null -ne $_ -and $null -ne $_.Name -and $_.Name -eq 'AdvisorScore'} | Select-Object -First 1
-        if ($advisorScoreSheet) {
-            try {
-                $Worksheets.MoveAfter('AdvisorScore', 'Overview')
-            } catch {
-                Write-Debug "  Warning: Could not move AdvisorScore sheet: $_"
-            }
-        }
-        
-        $supportTicketsSheet = $Worksheets | Where-Object { $null -ne $_ -and $null -ne $_.Name -and $_.Name -eq 'Support Tickets'} | Select-Object -First 1
-        if ($supportTicketsSheet) {
-            try {
-                $Worksheets.MoveAfter('Support Tickets', 'Overview')
-            } catch {
-                Write-Debug "  Warning: Could not move Support Tickets sheet: $_"
-            }
-        }
-        
-        $reservationSheet = $Worksheets | Where-Object { $null -ne $_ -and $null -ne $_.Name -and $_.Name -eq 'Reservation Advisor'} | Select-Object -First 1
-        if ($reservationSheet) {
-            try {
-                $Worksheets.MoveAfter('Reservation Advisor', 'Overview')
-            } catch {
-                Write-Debug "  Warning: Could not move Reservation Advisor sheet: $_"
-            }
-        }
-        
-        $subscriptionsSheet = $Worksheets | Where-Object { $null -ne $_ -and $null -ne $_.Name -and $_.Name -eq 'Subscriptions'} | Select-Object -First 1
-        if ($subscriptionsSheet) {
-            try {
-                $Worksheets.MoveAfter('Subscriptions','Overview')
-            } catch {
-                Write-Debug "  Warning: Could not move Subscriptions sheet: $_"
+        $overviewSheet = $Excel.Workbook.Worksheets['Overview']
+        if ($null -ne $overviewSheet) {
+            $overviewPosition = $overviewSheet.Position
+            $currentPosition = $overviewPosition + 1
+            
+            # Define sheets to move after Overview in order
+            $sheetsToMove = @('Advisor', 'Policy', 'Security Center', 'Quota Usage', 'AdvisorScore', 'Support Tickets', 'Reservation Advisor', 'Subscriptions')
+            
+            foreach ($sheetName in $sheetsToMove) {
+                $sheet = $Excel.Workbook.Worksheets[$sheetName]
+                if ($null -ne $sheet) {
+                    try {
+                        $sheet.Position = $currentPosition
+                        $currentPosition++
+                    } catch {
+                        Write-Debug "  Warning: Could not move $sheetName sheet: $_"
+                    }
+                }
             }
         }
     }
